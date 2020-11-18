b83aa5ea83110374aae47d3da5007dcd7df030a4
Alex S <alextheprogrammer21@gmail.com>
2020-11-18 11:43:00 -0500

    Updates

---
+++ precache-manifest.eb848e40060673ad202a466c18e02bc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e059aeb033ae73e0a74b1bb1ee7d3c",
+    "url": "/blog-searcher/index.html"
+  },
+  {
+    "revision": "e2894061dc312e3c6982",
+    "url": "/blog-searcher/static/css/main.f88cdd8f.chunk.css"
+  },
+  {
+    "revision": "71701d11664f43c8f566",
+    "url": "/blog-searcher/static/js/2.dba5ec5e.chunk.js"
+  },
+  {
+    "revision": "e2894061dc312e3c6982",
+    "url": "/blog-searcher/static/js/main.fda15336.chunk.js"
+  },
+  {
+    "revision": "3b825492cb1164bc9103",
+    "url": "/blog-searcher/static/js/runtime~main.a4b25977.js"
+  }
+]);
